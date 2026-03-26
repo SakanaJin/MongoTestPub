1a7012d3de13d3ebc1a6658018e3bb0a0adba51d
SakanaJin <144868138+SakanaJin@users.noreply.github.com>
2026-03-26 19:45:20 +0000

    yese

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,9 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://<user>:<password>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+//const uri = "mongodb+srv://<user>:<password>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+require("dotenv").config()
+const uri = process.env.DBSTRING
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
@@ -36,8 +38,8 @@ console.log("Looking for: " + searchKey);
 
 async function run() {
   try {
-    const database = client.db('ckmdb');
-    const parts = database.collection('cmps415');
+    const database = client.db('assignmentdb');
+    const parts = database.collection('mystuff');
 
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
